216ba31ff204df6dcdc1bd837b3751103af464e8
Andreas Kournoutas <andreaskournoutas@gmail.com>
2019-06-18 00:11:40 +0300

    Change "off-canvas" class to id

--- index.php
+++ index.php
@@ -65,7 +65,7 @@
 		<footer>
 			<jdoc:include type="modules" name="footer" style="xhtml"/>
 		</footer>
-		<div class="off-canvas">
+		<div id="off-canvas">
 			<jdoc:include type="modules" name="off-canvas" style="xhtml"/>
 		</div>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
