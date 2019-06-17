2b1e0d45cd5904edd8e12bfecabc53f3393fccc8
Andreas Kournoutas <andreaskournoutas@gmail.com>
2019-06-17 20:50:42 +0300

    Change fontawesome script placement

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/andreaskournoutas/utilities.css/utilities.min.css"/>
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css?v=<?php echo $cache; ?>"/>
+		<script src="https://kit.fontawesome.com/37e9037a2d.js"></script>
 	</head>
 	<body class="<?php echo $class ?>">
 		<div class="visible-xl d-none d-xl-block"></div>
@@ -68,7 +69,6 @@
 			<jdoc:include type="modules" name="off-canvas" style="xhtml"/>
 		</div>
 		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-		<script src="https://kit.fontawesome.com/37e9037a2d.js"></script>
 		<script type="text/javascript" src="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/js/template.js?v=<?php echo $cache; ?>"></script>
 	</body>
 </html>
