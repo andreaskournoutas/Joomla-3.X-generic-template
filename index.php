3c0e02ebb6b3767daca833c9bd71e43a796d0170
Andreas Kournoutas <andreaskournoutas@gmail.com>
2019-07-17 08:58:22 +0300

    Various modifications
    Remove Fontawesome from core project, add Google fonts placeholder, update utilities.css version

--- index.php
+++ index.php
@@ -25,10 +25,10 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
 		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
 		<jdoc:include type="head"/>
+		<!-- Google font link -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/andreaskournoutas/utilities.css/utilities.min.css"/>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/andreaskournoutas/utilities.css@2019.07.14/utilities.min.css"/>
 		<link rel="stylesheet" href="<?php echo $this->baseurl ?>/templates/<?php echo $this->template; ?>/css/template.css?v=<?php echo $cache; ?>"/>
-		<script src="https://kit.fontawesome.com/37e9037a2d.js"></script>
 	</head>
 	<body class="<?php echo $class ?>">
 		<div class="visible-xl d-none d-xl-block"></div>
